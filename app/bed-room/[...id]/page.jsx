0a226225a558ad9265acc9e05b2b10c784bf66e7
AndroIssaq <androashk88@gmail.com>
2023-10-22 13:04:20 +0300

    new2

--- app/bed-room/[...id]/page.jsx
+++ app/bed-room/[...id]/page.jsx
@@ -1,66 +1,65 @@
-import React from 'react'
 import { createClient } from 'contentful';
 import Image from 'next/image';
 import SingleProduct from '@/components/SingleProduct';
 import RelatedProducts from '@/components/RelatedProducts';
 
-export async function generateStaticParams() {
+export async function getStaticPaths() {
   try {
     const client = createClient({
-      space: 'wqjm0ul81wfi',
-      environment: 'master', // defaults to 'master' if not set
-      accessToken: '1HOop-wSR5WmMg-bEn7Kythm5IZVtZijsDiW5qjAbFk'
+      space: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
     });
     const res = await client.getEntries({ content_type: 'bedRooms' });
-    return res.items.map((item) => ({ id: Array.from([item.sys.id]) }));
+    const paths = res.items.map((item) => ({
+      params: { id: item.sys.id },
+    }));
+    return { paths, fallback: true };
   } catch (err) {
-    // Return a fallback page if an error occurs
     console.error('Error fetching data:', err);
-    return [{ id: ['fallback'] }];
+    return { paths: [], fallback: true };
   }
-}
-const getSingleProduct=async ({id})=>{
-  try {
-    console.log('id:',id);
-    const client = createClient({
-      space: 'wqjm0ul81wfi',
-      environment: 'master', // defaults to 'master' if not set
-      accessToken: '1HOop-wSR5WmMg-bEn7Kythm5IZVtZijsDiW5qjAbFk'
-    })
-    const response=await client.getEntries({content_type:'bedRooms','sys.id':id})
-    return response.items
-  } catch (error) {
-    console.log(error)
-  }
- 
 }
 
-const getRelatedProducts=async ({id})=>{
+export async function getStaticProps({ params }) {
   try {
     const client = createClient({
-      space: 'wqjm0ul81wfi',
-      environment: 'master', // defaults to 'master' if not set
-      accessToken: '1HOop-wSR5WmMg-bEn7Kythm5IZVtZijsDiW5qjAbFk'
-    })
-    const response=await client.getEntries({content_type:'bedRooms'})
-    const relatedItems=response.items.filter((item,index)=>item.sys.id!==id)
-    console.log(relatedItems);
-    return relatedItems
-  } catch (error) {
-    console.log(error)
+      space: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    });
+    const res = await client.getEntries({
+      content_type: 'bedRooms',
+      'sys.id': params.id,
+    });
+    if (!res.items.length) {
+      return { notFound: true };
+    }
+    const bedroom = res.items[0];
+    return {
+      props: { bedroom },
+      revalidate: 10,
+    };
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    return { notFound: true };
   }
- 
 }
-const BedRooms = async({params}) => {
-  const id=params.id[0]
-const data= await getSingleProduct({id})
-const relatedProducts=await getRelatedProducts({id})
+
+const BedRooms = ({ bedroom }) => {
+  const { name, images, price, description } = bedroom.fields;
+  const imageData = images[0].fields.file;
   return (
     <>
-    <SingleProduct data={data} />
-    <RelatedProducts relatedProducts={relatedProducts} page={'bed-room'}/>
-  </>
-  )
-}
+      <SingleProduct
+        name={name}
+        price={price}
+        description={description}
+        image={imageData}
+      />
+      <RelatedProducts page="bed-room" id={bedroom.sys.id} />
+    </>
+  );
+};
 
-export default BedRooms
+export default BedRooms;
